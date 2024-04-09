354526589d284b92288876adfed737e81de05226
Kiera P <01134.world@gmail.com>
2024-04-09 18:11:59 -0400

    Particles and bullet updates

--- particle_system.cpp
+++ particle_system.cpp
@@ -51,8 +51,11 @@ void ParticleSystem::Render(glm::mat4 view_matrix, double current_time){
 
     try {
         // Set up the parent transformation matrix
-        glm::mat4 parent_rotation_matrix = glm::rotate(glm::mat4(1.0f), parent_->GetRotation(), glm::vec3(0.0, 0.0, 1.0));
-        glm::mat4 parent_translation_matrix = glm::translate(glm::mat4(1.0f), parent_->GetPosition());
+        glm::mat4 parent_rotation_matrix = glm::rotate(glm::mat4(1.0f), parent_->GetRotation(), glm::vec3(0.0, 0.0, 1.0));\
+        // Get the parent's position but modify the z value to be 0
+        glm::vec3 parent_position = parent_->GetPosition();
+        parent_position.z = 0.0f;
+        glm::mat4 parent_translation_matrix = glm::translate(glm::mat4(1.0f), parent_position);
         glm::mat4 parent_transformation_matrix = parent_translation_matrix * parent_rotation_matrix;
 
         transformation_matrix = parent_transformation_matrix * translation_matrix * rotation_matrix * scaling_matrix;
